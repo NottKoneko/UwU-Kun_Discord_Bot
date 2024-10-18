ca05a4dbb498a06a63fc45dff84a43db1adca5b9
Koneko <pumasmichael@yahoo.com>
2024-10-18 18:10:50 +0000

    ughh

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const { loadCommands } = require('./commandHandler');
 const { createClient } = require('@supabase/supabase-js');
 
 // Initialize Supabase client
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.DB_URL;
+const supabaseKey = process.env.SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Import Discord.js classes and constants
