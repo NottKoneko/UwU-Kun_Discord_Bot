b17d7818cc3e742adfd7f821303f4e667c5ed4aa
Koneko <pumasmichael@yahoo.com>
2024-10-18 07:53:09 +0000

    Corrected import to const

--- DataBaseInit.js
+++ DataBaseInit.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = 'https://cslrxlgzmosivfgvzhmh.supabase.co'
 const supabaseSRKey = process.env.SERVICE_ROLE_KEY
