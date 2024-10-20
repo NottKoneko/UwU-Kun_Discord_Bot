0fcf9345895f3e2db2ad05f2f926d75dbad60cf3
Koneko <pumasmichael@yahoo.com>
2024-10-20 06:21:54 +0000

    Chambed filter to format

--- services/ytDlp.js
+++ services/ytDlp.js
@@ -3,9 +3,10 @@ const ytdl = require('yt-dlp-exec');
 // Function to stream audio from a YouTube URL
 function downloadAudio(youtubeUrl) {
   return ytdl(youtubeUrl, {
-    filter: 'audioonly',
+    format: 'bestaudio', // Replace 'filter' with 'format' and specify audio only
     quality: 'highestaudio'
   });
 }
 
 module.exports = { downloadAudio };
+
