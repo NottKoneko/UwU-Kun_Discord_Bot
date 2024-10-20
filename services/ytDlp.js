edb746669eed42ae749ded04f40099edaefaebde
Koneko <pumasmichael@yahoo.com>
2024-10-20 07:08:43 +0000

    Ugh

--- services/ytDlp.js
+++ services/ytDlp.js
@@ -1,11 +1,12 @@
 const ytdl = require('yt-dlp-exec');
 const path = require('path');
 
-// Function to stream audio from a YouTube URL
+// Function to stream audio from a YouTube URL using cookies
 function downloadAudio(youtubeUrl) {
   return ytdl(youtubeUrl, {
     format: 'bestaudio',
-    cookies: path.resolve(__dirname, './services/cookies.txt')  // Add path to cookies file
+    cookies: path.resolve(__dirname, 'cookies.txt'),  // Correct path to cookies.txt
+    execPath: path.resolve(__dirname, '../node_modules/yt-dlp-exec/bin/yt-dlp')  // Ensure the correct path to yt-dlp binary
   });
 }
 
