abf693e9f03ce65700f00b982bf222a883078053
Koneko <pumasmichael@yahoo.com>
2024-10-22 11:52:13 +0000

    jnmfe

--- commands/play.js
+++ commands/play.js
@@ -14,11 +14,19 @@ module.exports = {
         const trackName = interaction.options.getString('track_name');
         const voiceChannel = interaction.member.voice.channel;
 
+        // Check if the user is in a voice channel
         if (!voiceChannel) {
-            return interaction.reply('You need to be in a voice channel to play music!');
+            return interaction.reply({
+                content: 'You need to be in a voice channel to play music!',
+                ephemeral: true, // Show only to the user
+            });
         }
 
         try {
+            // Defer reply as searching can take some time
+            await interaction.deferReply();
+
+            // Create or get the player for this guild
             let player = interaction.client.moonlink.createPlayer({
                 guildId: interaction.guild.id,
                 voiceChannelId: voiceChannel.id,
@@ -26,39 +34,55 @@ module.exports = {
                 autoPlay: true,
             });
 
+            // Connect the player to the voice channel if it's not connected
             if (!player.connected) {
                 await player.connect({
-                    setDeaf: true,  // Deafen the bot
+                    setDeaf: true,  // Deafen the bot to avoid echo
                 });
             }
 
+            // Search for the track using Moonlink.js
             const res = await interaction.client.moonlink.search({
                 query: trackName,
-                source: 'youtube',  // Use YouTube as the source
+                source: 'youtube',  // You can change this to another source if needed
                 requester: interaction.user.id,
             });
 
+            // Handle search results
             if (res.loadType === 'loadfailed') {
-                return interaction.reply('Error: Failed to load the requested track.');
+                return interaction.editReply({
+                    content: 'Error: Failed to load the requested track.',
+                });
             } else if (res.loadType === 'empty') {
-                return interaction.reply('Error: No results found for the query.');
+                return interaction.editReply({
+                    content: 'Error: No results found for your query.',
+                });
             }
 
+            // Handle if a playlist is returned
             if (res.loadType === 'playlist') {
-                interaction.reply(`Playlist ${res.playlistInfo.name} has been added to the queue.`);
-                res.tracks.forEach(track => player.queue.add(track));
+                res.tracks.forEach(track => player.queue.add(track)); // Add all tracks from the playlist to the queue
+                await interaction.editReply({
+                    content: `Playlist **${res.playlistInfo.name}** has been added to the queue with **${res.tracks.length}** tracks.`,
+                });
             } else {
+                // Add a single track to the queue
                 player.queue.add(res.tracks[0]);
-                interaction.reply(`Track added to the queue: ${res.tracks[0].title}`);
+                await interaction.editReply({
+                    content: `Track **${res.tracks[0].title}** has been added to the queue.`,
+                });
             }
 
+            // Start playing if the player is not already playing
             if (!player.playing) {
                 player.play();
             }
 
         } catch (error) {
             console.error('Error playing music:', error);
-            interaction.reply('There was an error playing the song.');
+            interaction.editReply({
+                content: 'There was an error while trying to play the track. Please try again later.',
+            });
         }
     },
 };
